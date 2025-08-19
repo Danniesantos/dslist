91ceacf811c785da80bd524da8c46aea5b4d9f43
Danniesantos <danielardesantos@gmail.com>
2025-08-19 14:55:24 -0300

    Domain model

--- src/main/java/com/daniela/dsList/controller/GameController.java
+++ src/main/java/com/daniela/dsList/controller/GameController.java
@@ -1,7 +1,6 @@
 package com.daniela.dsList.controller;
 
 import com.daniela.dsList.dto.GameMinDTO;
-import com.daniela.dsList.entities.Game;
 import com.daniela.dsList.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
